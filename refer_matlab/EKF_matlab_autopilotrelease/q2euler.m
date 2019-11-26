7f1ee50d98f165d284b85feadce26128c3d7b9e0
xhzhuhit <xhzhuhit@163.com>
2019-11-26 22:02:30 +0800

    add matlab code as reference

--- refer_matlab/EKF_matlab_autopilotrelease/q2euler.m
+++
@@ -1,9 +0,0 @@
-function [euler]=q2euler(q)
-
-%     glvs
-%     global q;
-    euler=[ atan2((2.0 * (q(1) * q(2) + q(3) * q(4))), 1.0 - 2.0 * (q(2) * q(2) + q(3) * q(3)));
-            asin(2.0 * (q(1) * q(3) - q(4) * q(2)));
-            atan2(2.0 * (q(1) * q(4) + q(2) * q(3)), 1.0 - 2.0 * (q(3) * q(3) + q(4) * q(4)))];
-
-end
