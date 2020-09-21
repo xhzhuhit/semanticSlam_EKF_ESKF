87a0aac939d121025819fc58b25cf64a022d1c84
zhuxiaohui <zhuxiaohui@baidu.com>
2020-09-21 10:38:19 +0800

    修改ESKF,prediction阶段直接乘以F.可以正常运行，6-8ｗ帧有航向跳动，可能还有问题.需要查一下.

--- ESKF/ESKF.py
+++ ESKF/ESKF.py
@@ -72,7 +72,7 @@ for i in range(gps_buf, len_data):
         roll = math.atan2(np.mean(acc[100:150,1]), np.mean(acc[100:150, 2]))
         yaw = gps_heading[i]
         x_nom[6:10] = euler2q([roll, pitch, yaw])
-        rpy = q2euler(x_nom[0:4])
+        rpy = q2euler(x_nom[6:10])
         print "yaw = %f, rpy = %f, %f, %f" %(yaw, rpy[0], rpy[1], rpy[2])
         #print x[0:4]
         P = np.diag([0.25, 0.25, 0.25, 0.01, 0.01, 0.01, 0, 0, 0, 
@@ -82,6 +82,7 @@ for i in range(gps_buf, len_data):
         filter_init = 1
         x_nom[3:6] = v[i]
         x_nom[0:3] = gps[i,3:6]
+        #a_b and w_b is 0, so do not do init
     #end fi filter_init
     dt = (timestamp[i] - timestamp[i-1]) / 1e6
 
@@ -98,35 +99,41 @@ for i in range(gps_buf, len_data):
                 x_nom[6] * dq[3] + x_nom[7] * dq[2] - x_nom[8] * dq[1] + x_nom[9] * dq[0]]
         x_nom[6:10] = x_nom[6:10] / np.linalg.norm(x_nom[6:10])
 
-        Vel_pre = x_nom[3:6]
+        Vel_pre = x_nom[3:6].copy()
         x_nom[3:6] = x_nom[3:6] + delVel
         x_nom[0:3] = x_nom[0:3] + (x_nom[3:6] + Vel_pre) * dt/2
         #predict error state
-        x_err[9:12] = x_err[9:12]
-        x_err[12:15] = x_err[12:15]
-        R = q2R(dq)
-        x_err[6:9] = np.dot(R.transpose(), x_err[6:9]) - x_err[12:15] * dt
-        tmp_Rx = np.dot(Cbn, euler2skewSymmetric(gyro[i,:].transpose() - x_nom[13:16]))
-        x_err[3:6] = x_err[3:6] + (- np.dot(tmp_Rx, x_err[6:9]) - np.dot(Cbn, x_err[9:12])) * dt
-        x_err[0:3] = x_err[0:3] + x_err[3:6] * dt
+        #under predict is wrong, 267 in book is simply update
+        #x_err[9:12] = x_err[9:12]
+        #x_err[12:15] = x_err[12:15]
+        #R = q2R(dq)
+        #x_err[6:9] = np.dot(R.transpose(), x_err[6:9]) - x_err[12:15] * dt
+        #x_err[3:6] = x_err[3:6] + (- np.dot(tmp_Rx, x_err[6:9]) - np.dot(Cbn, x_err[9:12])) * dt
+        #x_err[0:3] = x_err[0:3] + x_err[3:6] * dt
         #end predict, then need to update P and Q
+        tmp_Rx = np.dot(Cbn, euler2skewSymmetric(acc[i,:].transpose() - x_nom[10:13]))
+        delta_theta = (gyro[i,:].transpose() - x_nom[13:16]) * dt
+        tmp_Rr = q2R(euler2q(delta_theta))
+
         FF = np.zeros((15,15))
         FF[0:3,0:3] = np.eye(3)
         FF[0:3,3:6] = np.eye(3) * dt
         FF[3:6,3:6] = np.eye(3)
         FF[3:6,6:9] = - tmp_Rx * dt
         FF[3:6,9:12] = - Cbn * dt
-        FF[6:9,6:9] = R
+        FF[6:9,6:9] = tmp_Rr.transpose()
         FF[6:9,12:15] = - np.eye(3) * dt
         FF[9:12,9:12] = np.eye(3)
         FF[12:15,12:15] = np.eye(3)
 
+        #do error state predict
+        x_err = np.dot(FF, x_err)
 
         F= np.array(FF)
         #print F.shape
-        Q = np.diag([gyrox_noise, gyroy_noise, gyroz_noise,accx_noise, accy_noise, accz_noise,gyrox_bias_noise, gyroy_bias_noise, gyroz_bias_noise,accx_bias_noise, accy_bias_noise, accz_bias_noise])
+        Q = np.diag([gyrox_noise, gyroy_noise, gyroz_noise, accx_noise, accy_noise, accz_noise,gyrox_bias_noise, gyroy_bias_noise, gyroz_bias_noise, accx_bias_noise, accy_bias_noise, accz_bias_noise])
         Fi = np.zeros((15,12))
-        Fi[0:3,3:6] = np.eye(3) * dt
+        #Fi[0:3,3:6] = np.eye(3) * dt
         Fi[3:15,0:12] = np.eye(12)
         Fi = np.array(Fi)
         Q = np.dot(np.dot(Fi, Q), Fi.transpose())
@@ -139,81 +146,85 @@ for i in range(gps_buf, len_data):
     fusion_heading[i] = rpy[2]
 
 #2019-11-24 20:09
+    #if have gps data, then do follow, else just say continue to return to the begin
+    #if gps_heading[i] != gps_heading[i-1]:
     #use gps data: speed_n speed_e speed_d x y yaw
     #calc H
-    H_x = np.zeros((15,16))
-    H_x[0,0] = 1
-    H_x[1,1] = 1
-    H_x[3,3] = 1
-    H_x[4,4] = 1
-    H_x[5,5] = 1
+    if math.fabs(gps[i,3] - gps[i-1,3]) > 0.000001:
+        H_x = np.zeros((15,16))
+        H_x[0,0] = 1
+        H_x[1,1] = 1
+        H_x[3,3] = 1
+        H_x[4,4] = 1
+        H_x[5,5] = 1
 
-    euler = q2euler(x_nom[6:10])
-    t1 = 2*(x_nom[6]*x_nom[9] + x_nom[7]*x_nom[8])
-    t2 = pow(x_nom[6],2) + pow(x_nom[7],2) - pow(x_nom[8],2) - pow(x_nom[9],2)
-    t3 = t1 / t2
-    t4 = 1 / (1 + pow(t3,2))
-    t5 = pow(t2,2)
-    t6 = t4 / t5
-    H_x[8,6:10] = [t6*(2*x_nom[9]*t2 - 2*x_nom[6]*t1), t6*(2*x_nom[8]*t2 - 2*x_nom[7]*t1), 
-                    t6*(2*x_nom[7]*t2 + 2*x_nom[8]*t1), t6*(2*x_nom[6]*t2 + 2*x_nom[9]*t1)]
+        euler = q2euler(x_nom[6:10])
+        t1 = 2*(x_nom[6]*x_nom[9] + x_nom[7]*x_nom[8])
+        t2 = pow(x_nom[6],2) + pow(x_nom[7],2) - pow(x_nom[8],2) - pow(x_nom[9],2)
+        t3 = t1 / t2
+        t4 = 1 / (1 + pow(t3,2))
+        t5 = pow(t2,2)
+        t6 = t4 / t5
+        H_x[8,6:10] = [t6*(2*x_nom[9]*t2 - 2*x_nom[6]*t1), t6*(2*x_nom[8]*t2 - 2*x_nom[7]*t1), 
+                        t6*(2*x_nom[7]*t2 + 2*x_nom[8]*t1), t6*(2*x_nom[6]*t2 + 2*x_nom[9]*t1)]
 
-    X_dx = np.zeros((16,15))
-    X_dx[0:6,0:6] = np.eye(6)
-    X_dx[10:16,9:15] = np.eye(6)
-    qw,qx,qy,qz = x_nom[6:10]
+        #calc X_dx
+        X_dx = np.zeros((16,15))
+        X_dx[0:6,0:6] = np.eye(6)
+        X_dx[10:16,9:15] = np.eye(6)
+        qw,qx,qy,qz = x_nom[6:10]
+        
+        mm = [[-qx, -qy, -qz], 
+              [ qw, -qz,  qy], 
+              [ qz,  qw, -qx], 
+              [-qy,  qx,  qw]]
+        X_dx[6:10,6:9] = 0.5 * np.array(mm) 
+        H = np.dot(H_x, X_dx)
+        
+        # calc K, update P, and calc delta_x
+        V = np.eye(15) * 1e-9
+        nom = np.dot(P,H.transpose())
+        denom = np.dot(np.dot(H,P), H.transpose()) + V
+        K = np.dot(nom, np.linalg.pinv(denom))
+        y = [gps[i,3], gps[i,4], 0, gps[i,0], gps[i,1], gps[i,2], 0, 0, gps_heading[i], 0,0,0,0,0,0]
+        rpy0 = q2euler(x_nom[6:10])
+        h_xv = [x_nom[0], x_nom[1], 0, x_nom[3], x_nom[4], x_nom[5], 0, 0, rpy0[2], 0,0,0,0,0,0]
+        a = np.array(y) - np.array(h_xv)
+        print y
+        print h_xv
+        print "minus residual:"
+        print a
+        print K
+        delta_x = np.dot(K, np.array(y) - np.array(h_xv))
     
-    mm = [[-qx, -qy, -qz], 
-          [ qw, -qz,  qy], 
-          [ qz,  qw, -qx], 
-          [-qy,  qx,  qw]]
-    X_dx[6:10,6:9] = 0.5 * np.array(mm) 
-    H = np.dot(H_x, X_dx)
-    
-    # calc K, update P, and calc delta_x
-    V = np.eye(15) * 1e-9
-    nom = np.dot(P,H.transpose())
-    denom = np.dot(np.dot(H,P), H.transpose()) + V
-    K = np.dot(nom, np.linalg.pinv(denom))
-    y = [gps[i,3], gps[i,4], 0, gps[i,0], gps[i,1], gps[i,2], 0, 0, gps_heading[i], 0,0,0,0,0,0]
-    rpy0 = q2euler(x_nom[6:10])
-    h_xv = [x_nom[0], x_nom[1], 0, x_nom[3], x_nom[4], x_nom[5], 0, 0, rpy0[2], 0,0,0,0,0,0]
-    a = np.array(y) - np.array(h_xv)
-    print y
-    print h_xv
-    print "minus residual:"
-    print a
-    print K
-    delta_x = np.dot(K, np.array(y) - np.array(h_xv))
-    
-    gps_correct = 1
-    if gps_correct:
-        #do update, injection of observed error into nominal state
-        print delta_x
-        x_nom[0:6] += delta_x[0:6]
-        delta_q = euler2q(delta_x[6:9])
-        #print delta_q
-        drpy1 = q2euler(x_nom[6:10])
-        x_nom[6:10] = qUpdateDq(x_nom[6:10], delta_q)
-        x_nom[6:10] = x_nom[6:10] / np.linalg.norm(x_nom[6:10])
-        drpy2 = q2euler(x_nom[6:10])
-        delta_drpy = np.array(drpy2) - np.array(drpy1)
-        if delta_drpy[2] > 0.1:
-            print "Divergence !"
-        print drpy1
-        print drpy2
+        gps_correct = 1
+        if gps_correct:
+            #do update, injection of observed error into nominal state
+            print delta_x
+            x_nom[0:6] += delta_x[0:6]
+            delta_q = euler2q(delta_x[6:9])
+            #print delta_q
+            drpy1 = q2euler(x_nom[6:10])
+            x_nom[6:10] = qUpdateDq(x_nom[6:10], delta_q)
+            x_nom[6:10] = x_nom[6:10] / np.linalg.norm(x_nom[6:10])
+            drpy2 = q2euler(x_nom[6:10])
+            delta_drpy = np.array(drpy2) - np.array(drpy1)
+            if delta_drpy[2] > 0.1:
+                print "Divergence !"
+            print drpy1
+            print drpy2
 
-        #do ESKF reset
-        '''
-        G = np.eye(15)
-        G[0:6,0:6] = np.eye(6)
-        G[9:15,9:15] = np.eye(6)
-        G[6:9,6:9] = np.eye(3) - euler2skewSymmetric(0.5*delta_x[6:9]) 
-        P = np.dot(np.dot(G,P), G.transpose())
-        '''
+            #do ESKF reset
+            '''
+            G = np.eye(15)
+            G[0:6,0:6] = np.eye(6)
+            G[9:15,9:15] = np.eye(6)
+            G[6:9,6:9] = np.eye(3) - euler2skewSymmetric(0.5*delta_x[6:9]) 
+            P = np.dot(np.dot(G,P), G.transpose())
+            '''
 
 lmin = 10
-lmax = 500 #2000
+lmax = 90000 #2000
 plt.plot([gps[i,3] for i in range(lmin,lmax)], [gps[i,4] for i in range(lmin,lmax)], '-')
 plt.plot([fusion_pose[i,0] for i in range(lmin,lmax)], [fusion_pose[i,1] for i in range(lmin,lmax)], '-r')
 plt.legend(['gps_position', 'fusion_position'])
